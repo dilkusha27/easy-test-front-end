f2f632e81343122d7a9cbd4a3a9a47e53d2a816a
dilkusha27 <dilkusha27@gmail.com>
2025-10-12 21:59:53 +0900

    chore: 의존성 추가 및 라우팅 구현, 스타일 개선, 코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import UploadFileDropPage from './components/pages/UploadFileDropPage';
+import UploadProgressPage from './components/pages/UploadProgressPage';
+import ReportPage from './components/pages/ReportPage';
+import UploadCompletePage from './components/pages/UploadCompletePage';
+import TestProgressPage from './components/pages/TestProgressPage';
+import TestCompletePage from './components/pages/TestCompletePage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/upload" element={<UploadFileDropPage />} />
+        <Route path="/uploading" element={<UploadProgressPage />} />
+        <Route path="/uploaded" element={<UploadCompletePage />} />
+        <Route path="/testing" element={<TestProgressPage />} />
+        <Route path="/tested" element={<TestCompletePage />} />
+        <Route path="/reports" element={<ReportPage />} />
+        <Route path="*" element={<Navigate to="/upload" replace />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
