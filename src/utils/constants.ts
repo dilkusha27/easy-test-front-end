9d34f1dd6309e497290dd86075443a2baece8e23
dilkusha27 <dilkusha27@gmail.com>
2025-10-15 05:58:31 +0900

    chore: .env 파일 및 API_BASE_URL 상수 정의 추가

---
+++ src/utils/constants.ts
@@ -0,0 +1,9 @@
+// 상수 정의 (문구, 색상 등)
+
+// 왜 있는지 모르겠음
+// export const COLORS = {
+//   primary: '#007bff',
+//   secondary: '#6c757d',
+// };
+
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
